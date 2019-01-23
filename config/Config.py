8bcbe26f61978339f2f2dbb09d6206e75d9bae60
pengming <549639484@qq.com>
2019-01-22 21:41:59 -0800

    修改参数

--- config/Config.py
+++ config/Config.py
@@ -6,7 +6,7 @@ class Config(object):
     def __init__(self):
         self.Batch_Size = 32
         self.epoch = 20
-        self.is_cut = False  # 是否对语句进行分词
+        self.is_cut = True  # 是否对语句进行分词
 
 
 if __name__ == '__main__':
